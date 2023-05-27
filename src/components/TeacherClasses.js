55de4fdddc9c91e25abc37cff5058cafa2023f39
ohayonyo <ohayonyo@post.bgu.ac.il>
2023-05-28 01:10:54 +0300

    add background template to teacherMenu/myClasses

--- src/components/TeacherClasses.js
+++ src/components/TeacherClasses.js
@@ -122,70 +122,84 @@ export default function TeacherClasses() {
   }
 
   return (
-    <div class="class-list" style={{resize: 'both', overflow: 'auto', width:'105%', paddingRight:'20%'}}>
-  <React.Fragment>
-    <CssBaseline />
-    <Paper square sx={{ pb: '50px' }}>
-      <Typography variant="h5" gutterBottom component="div" sx={{ p: 2, pb: 0 }} style={{textAlign:'center',marginRight:-100}}>
-        הכיתות שלי
-      </Typography>
-      <div>
-        <List sx={{ mb: 2 }}>
-          {messages.map(({ id,primary, secondary }) => (
-            <React.Fragment key={id}>
-              <ListItem 
-                  Button 
-                  onClick={(event) => {
-                    if (
-                      event.target.tagName !== "BUTTON" &&
-                      event.target.tagName !== "svg" &&
-                      event.target.tagName !== "path"
-                    ) {
-                      gotoUnits(id, event);
-                    }
-                  }}
-                >
-                  <IconButton edge="end" aria-label="delete" onClick={() => deleteElement(id)}>
-                    <DeleteIcon />
-                  </IconButton>
-                  {/* <IconButton edge="end" aria-label="edit" onClick={() => gotoEdit(id)}>
-                    <AddIcon />
-                  </IconButton> */}
-                  {/* <IconButton edge="end" aria-label="units" onClick={() => gotoUnits(id)}>
-                    <MenuIcon />
-                  </IconButton> */}
-                  <ListItemText
-                    primary={
-                      <Typography variant="h6" style={{ color: "#000000" }}>
-                        {primary}
-                      </Typography>
-                    }
-                    secondary={secondary}
-                    style={{ textAlign: "right", marginTop: -1, marginRight: 20 }}
-                  />
-                </ListItem>
-            </React.Fragment>
-          ))}
-        </List>
-      </div>
-    </Paper>  
-  </React.Fragment>
-  <div style={{ display: 'flex', justifyContent: 'center' }}>
-  <div style={{marginTop:15}}>
-      <button className="form-submit" onClick={()=>openClass()} style={{float: 'left',height:42}}>
-        הוספת כיתה חדשה
-      </button>
-    </div>
-    <input
-    type="text"
-    className='input_data'
-    style={{ color: 'black', height: 42, marginTop: 15, direction: 'rtl' }}
-    value={className}
-    onChange={(e) => setClassName(e.target.value)}
-    />
+    <div>
+          <div className='Home_page_background'></div>
 
-  </div>
-</div>
+         <div className="class-list" style={{resize: 'both', overflow: 'auto', width:'105%', paddingRight:'20%'}}>
+            <React.Fragment>
+              <CssBaseline />
+              <Paper square sx={{ pb: '50px' }}>
+                <Typography variant="h5" gutterBottom component="div" sx={{ p: 2, pb: 0 }} style={{textAlign:'center',marginRight:-100,position:'relative',zIndex:1}}>
+                  הכיתות שלי
+                </Typography>
+                <div style={{position:'relative',zIndex:1}}>
+                  <List sx={{ mb: 2 }}>
+                    {messages.map(({ id,primary, secondary }) => (
+                      <React.Fragment key={id}>
+                        <ListItem 
+                            Button 
+                            onClick={(event) => {
+                              if (
+                                event.target.tagName !== "BUTTON" &&
+                                event.target.tagName !== "svg" &&
+                                event.target.tagName !== "path"
+                              ) {
+                                gotoUnits(id, event);
+                              }
+                            }}
+                          >
+                            <IconButton edge="end" aria-label="delete" onClick={() => deleteElement(id)}>
+                              <DeleteIcon />
+                            </IconButton>
+                            {/* <IconButton edge="end" aria-label="edit" onClick={() => gotoEdit(id)}>
+                              <AddIcon />
+                            </IconButton> */}
+                            {/* <IconButton edge="end" aria-label="units" onClick={() => gotoUnits(id)}>
+                              <MenuIcon />
+                            </IconButton> */}
+                            <ListItemText
+                              primary={
+                                <Typography variant="h6" style={{ color: "#000000" }}>
+                                  {primary}
+                                </Typography>
+                              }
+                              secondary={secondary}
+                              style={{ textAlign: "right", marginTop: -1, marginRight: 20 }}
+                            />
+                          </ListItem>
+                      </React.Fragment>
+                    ))}
+                  </List>
+                </div>
+              </Paper>  
+            </React.Fragment>
+
+            {/* <p style={{ zIndex: 100, color: 'black', position: 'relative' }}>
+                hello world
+            </p> */}
+
+            <hr class="bar-line" style={{zIndex:1,position:'relative'}}></hr>
+
+            <div style={{ display: 'flex', justifyContent: 'center',position:'relative',zIndex:1}}>
+
+
+            <div style={{marginTop:15}}>
+                <button className="form-submit" onClick={()=>openClass()} style={{float: 'left',height:42}}>
+                  הוספת כיתה חדשה
+                </button>
+              </div>
+              <input
+              type="text"
+              className='input_data'
+              style={{ color: 'black', height: 42, marginTop: 15, direction: 'rtl' }}
+              value={className}
+              onChange={(e) => setClassName(e.target.value)}
+              />
+
+            </div>
+          </div>
+    </div>
+   
     
   );
 }
